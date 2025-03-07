ea68715d8c75917b760e93f6a0169d10ec59a118
Rafael Vescovi <ravescovi@gmail.com>
2025-03-07 12:55:57 -0600

    add test for RE(make_devices)

---
+++ src/instrument/tests/conftest.py
@@ -0,0 +1,17 @@
+from typing import Any
+import pytest
+
+from instrument.startup import make_devices, RE
+
+@pytest.fixture(scope="session")
+def runengine_with_devices() -> Any:
+    """
+    Initialize the RunEngine with devices for testing.
+
+    This fixture calls RE with the output of make_devices() to mimic
+    the behavior previously performed in the startup module.
+
+    Returns:
+        Any: An instance of the RunEngine with devices configured.
+    """
+    return RE(make_devices()) 
