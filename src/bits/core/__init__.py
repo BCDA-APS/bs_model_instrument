3e780d23a5251ff2d116993643e796b4ae21594a
Rafael Vescovi <ravescovi@gmail.com>
2025-03-14 17:28:17 -0500

    fix more relative imports

--- src/bits/core/__init__.py
+++ src/bits/core/__init__.py
@@ -4,10 +4,10 @@ Utility support to start bluesky sessions.
 Also contains setup code that MUST run before other code in this directory.
 """
 
-from ..utils.aps_functions import aps_dm_setup
-from ..utils.config_loaders import iconfig
-from ..utils.helper_functions import debug_python
-from ..utils.helper_functions import mpl_setup
+from bits.utils.aps_functions import aps_dm_setup
+from bits.utils.config_loaders import iconfig
+from bits.utils.helper_functions import debug_python
+from bits.utils.helper_functions import mpl_setup
 
 debug_python()
 mpl_setup()
